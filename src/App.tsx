09f167767907d99614b94fea9761008d6c5a6166
Kostya Karpin <4epubelik@gmail.com>
2024-07-16 22:06:24 +0300

    advanced react webpack setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
 
 const App = () => {
+
+    const [num, setNum] = useState(0);
+
     return(
-        <>
-        hello
-        </>
+        <div>
+            <h1>Entry point</h1>         
+        </div>
     )
 }
 
---
+++ webpack/webpack.common.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+// const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+// const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  name: 'client',
+  entry: {
+    'react-app-setup': path.resolve(__dirname, '..', './src/index.tsx'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    mainFields: ['source', 'module', 'main'],
+    modules: [path.resolve('node_modules')],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '..', './build'),
+    filename: '[name]_[fullhash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '..', './public/index.html'),
+      // favicon: path.resolve(__dirname, '..', './public/images/favicon.png'),
+      showErrors: true,
+    }),
+    // new WebpackManifestPlugin({
+    //   writeToFileEmit: true,
+    // }), 
+    new ForkTsCheckerWebpackPlugin(),
+    // new Dotenv({
+    //   path: '.env',
+    // }),
+  ],
+  stats: 'errors-only',
+};
