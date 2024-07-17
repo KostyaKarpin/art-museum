12989601ab2e62532de64b675b2fa6b661e586af
Kostya Karpin <4epubelik@gmail.com>
2024-07-17 14:34:34 +0300

    added hash-router and routs to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+// eslint-disable-next-line import/namespace
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import FavoritesPage from "./pages/FavoritesPage";
+
 const App = () => {
   return (
-    <div>
-      <h1>Entry point</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about/:id" element={<AboutPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+// eslint-disable-next-line import/namespace
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.querySelector("#root") as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <HashRouter basename="/">
+      <App />
+    </HashRouter>
   </StrictMode>
 );
