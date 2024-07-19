dc1fce967585b4d365fda2acce032045415344ff
Kostya Karpin <4epubelik@gmail.com>
2024-07-19 21:28:28 +0300

    feat: FavoritesPage component can read data (id) from local storage

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,7 +1,22 @@
+// import useLocalStorage from "react-use-localstorage";
+
+import { useState } from "react";
+
 const FavoritesPage = () => {
+  // const [storageItem] = useLocalStorage("Favorites");
+  const [storageItem] = useState(() => {
+    const saved = localStorage.getItem("Favorites") || "";
+    const initialValue = JSON.parse(saved);
+    return initialValue;
+  });
+  console.log(storageItem);
   return (
     <div>
       <h1>FavoritesPage</h1>
+      {storageItem &&
+        storageItem.map((item: number, index: number) => {
+          return <p key={index}>ID: {item}</p>;
+        })}
     </div>
   );
 };
