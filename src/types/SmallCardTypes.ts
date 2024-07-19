790dd5e4e217e6d8b8efff36720b77dd0d8dbcb6
Kostya Karpin <4epubelik@gmail.com>
2024-07-19 19:31:51 +0300

    feat: added for fetched data

---
+++ src/types/SmallCardTypes.ts
@@ -0,0 +1,11 @@
+export interface SmallCardsTypes {
+  id: number;
+  image_id: number;
+  title: string;
+  artist_title: string;
+  artist_display: string;
+  date_display: string;
+  dimensions: string;
+  credit_line: string;
+  is_public_domain: string;
+}
