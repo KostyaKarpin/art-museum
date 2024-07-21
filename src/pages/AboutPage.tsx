37b95532a9304c6bcab71a50b9ba7eff33c6d866
Kostya Karpin <4epubelik@gmail.com>
2024-07-21 20:53:01 +0300

    feat: AboutPage component gets clicked painting id from params and renders painting data

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,24 @@
+import useFetchByID from "@/hooks/useFetchByID";
+import { Painting } from "@/types/PaintingTypes";
+// eslint-disable-next-line import/namespace
+import { useParams } from "react-router-dom";
+
 const AboutPage = () => {
+  const { id } = useParams();
+  console.log("ID:" + id);
+
+  const data: Painting | undefined = useFetchByID(id!);
+  console.log(data);
+
   return (
     <div>
       <h1>AboutPage</h1>
+      <div>
+        <h1>{data && data.title}</h1>
+        <h2>{data && data.artist_title}</h2>
+        <h4>{data && data.artist_display}</h4>
+        <h6>-----------------------</h6>
+      </div>
     </div>
   );
 };
