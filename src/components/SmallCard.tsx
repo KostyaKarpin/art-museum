b818f4a8a07c347a0425e4c2c2c5ede22da5e5f9
Kostya Karpin <4epubelik@gmail.com>
2024-07-21 20:55:39 +0300

    refactor: SmallCard component now can redirect to AboutPage

--- src/components/SmallCard.tsx
+++ src/components/SmallCard.tsx
@@ -1,5 +1,7 @@
 import { Painting } from "@/types/PaintingTypes";
 import FavButton from "./FavButton";
+// eslint-disable-next-line import/namespace
+import { Link } from "react-router-dom";
 
 type Props = {
   painting: Painting;
@@ -8,7 +10,9 @@ type Props = {
 const SmallCard = (props: Props) => {
   return (
     <div>
-      <h6>{props.painting.title}</h6>
+      <Link to={`/about/:${props.painting.id}`}>
+        <h6>{props.painting.title}</h6>
+      </Link>
       <h6>{props.painting.artist_title}</h6>
       <h6>{props.painting.is_public_domain ? "Public" : "Private"}</h6>
       <FavButton painting={props.painting} />
