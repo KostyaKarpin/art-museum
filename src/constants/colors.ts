7d530b9e535c742a72fba70a19ef966dfa4e0470
Kostya Karpin <4epubelik@gmail.com>
2024-07-22 12:21:07 +0300

    feat: added colors constants file

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export const LIGHT_GRAY = "#F9F9F9";
+export const LIGHT_ORANGE = "#E0A449";
+export const GRAY = "#3939390D";
+export const BLACK = "#393939";
+export const WHITE = "#FAFAFA";
+export const BLACK_GRADIENT =
+  "90deg, #343333 38.05%, #484848 69.22%, #282828 98.98%";
