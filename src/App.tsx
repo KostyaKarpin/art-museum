e1365e2ef95245e86841536499f8c8f38ecb4b36
Kostya Karpin <4epubelik@gmail.com>
2024-07-22 13:16:45 +0300

    feat: added Global styled component for global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 // eslint-disable-next-line import/namespace
 import { Route, Routes } from "react-router";
 import { routes } from "@constants/routes";
+import { Global } from "./globalStyles/Global";
 
 const App = () => {
   const routeComponents = routes.map(({ path, component }, key) => {
     return <Route key={key} path={path} element={component} />;
   });
 
-  return <Routes>{routeComponents}</Routes>;
+  return (
+    <>
+      <Global />
+      <Routes>{routeComponents}</Routes>;
+    </>
+  );
 };
 
 export default App;
---
+++ src/globalStyles/Global.tsx
@@ -0,0 +1,13 @@
+import { BLACK } from "@/constants/colors";
+// eslint-disable-next-line import/namespace
+import { createGlobalStyle } from "styled-components";
+
+export const Global = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        color: ${BLACK}
+    }
+`;
