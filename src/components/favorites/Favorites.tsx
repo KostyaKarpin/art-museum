ede4041070ecdc6906d77640fb309d8702d9d09f
Kostya Karpin <4epubelik@gmail.com>
2024-07-23 16:02:05 +0300

    feat: added filters to favorite component

--- src/components/favorites/Favorites.tsx
+++ src/components/favorites/Favorites.tsx
@@ -7,16 +7,74 @@ import useLocalStorage from "react-use-localstorage";
 import { useRef } from "react";
 import PaintingCard from "../cards/painting/PaintingCard";
 import { Painting } from "@/types/PaintingTypes";
+// eslint-disable-next-line import/namespace
+import { Field, Form, Formik } from "formik";
 
 export const Favorites = () => {
-  const [storageItem] = useLocalStorage("Favorites", JSON.stringify([]));
+  const [storageItem, setStorageItem] = useLocalStorage(
+    "Favorites",
+    JSON.stringify([])
+  );
   const storageArray = useRef(JSON.parse(storageItem));
+
+  const handleChange = (value: string) => {
+    switch (value) {
+      case "Title": {
+        storageArray.current.sort((a: Painting, b: Painting) => {
+          return a.title && b.title ? a.title.localeCompare(b.title) : -1;
+        });
+        setStorageItem(JSON.stringify(storageArray.current));
+        break;
+      }
+      case "Artist": {
+        storageArray.current.sort((a: Painting, b: Painting) => {
+          return a.artist_title && b.artist_title
+            ? a.artist_title.localeCompare(b.artist_title)
+            : -1;
+        });
+        setStorageItem(JSON.stringify(storageArray.current));
+        break;
+      }
+      case "Public": {
+        storageArray.current.sort((a: Painting, b: Painting) => {
+          return +b.is_public_domain - +a.is_public_domain;
+        });
+        setStorageItem(JSON.stringify(storageArray.current));
+        break;
+      }
+    }
+    console.log(storageArray.current);
+  };
+
   return (
     <Wrapper>
       <Text>
-        Here are your <Image src={BookmarkIcon} width={"64px"} />{" "}
+        Here are your <Image src={BookmarkIcon} width={"32px"} />{" "}
         <Decorate>Favorites</Decorate>
       </Text>
+      <Formik
+        initialValues={{ filter: "Select option to filter" }}
+        onSubmit={() => console.log("Submitted")}
+      >
+        <Form>
+          <Field
+            as="select"
+            name="filter"
+            onChange={(event: { target: { value: string } }) =>
+              handleChange(event.target.value)
+            }
+          >
+            {["Select option to filter", "Title", "Artist", "Public"].map(
+              (item: string, index: number) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              )
+            )}
+          </Field>
+        </Form>
+      </Formik>
+
       <GridWrapper>
         {storageArray &&
           storageArray.current.map((painting: Painting, index: number) => {
--- src/components/favorites/FavoritesStyles.tsx
+++ src/components/favorites/FavoritesStyles.tsx
@@ -6,7 +6,7 @@ export const Wrapper = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 0 10%;
+  padding: 5% 10%;
 `;
 
 export const GridWrapper = styled.div`
